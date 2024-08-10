f8ed0634ee35b525fd156f59a0345873b62aecca
memothelemo <dev@memothelemo.xyz>
2024-08-11 03:17:13 +0800

    Remove unused imports

--- crates/eden-utils/src/build.rs
+++ crates/eden-utils/src/build.rs
@@ -1,6 +1,3 @@
-use chrono::NaiveDate;
-use std::sync::LazyLock;
-
 pub const COMMIT_BRANCH: &str = env!("VERGEN_GIT_BRANCH");
 pub const COMMIT_HASH: &str = env!("VERGEN_GIT_SHA");
 pub const PROFILE: &str = env!("BUILD_PROFILE");
